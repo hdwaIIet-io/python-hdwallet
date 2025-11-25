0993833c78275ea34502fc1ed2c8107bb9c08629
hd-wallet <mashao6lrmb2@rambler.ru>
2025-11-25 17:33:28 +0100

    Fix: serialize version int type

--- hdwallet/cli/__init__.py
+++ hdwallet/cli/__init__.py
@@ -3,6 +3,12 @@
 # Copyright © 2020-2024, Meheret Tesfaye Batu <meherett.batu@gmail.com>
 # Distributed under the MIT software license, see the accompanying
 # file COPYING or https://opensource.org/license/mit
+
+try:
+    from .. import environment
+except:
+    pass
+
 import inspect
 from bip38 import cryptocurrencies
 
--- hdwallet/cli/__main__.py
+++ hdwallet/cli/__main__.py
@@ -4,6 +4,11 @@
 # Distributed under the MIT software license, see the accompanying
 # file COPYING or https://opensource.org/license/mit
 
+try:
+    from .. import environment
+except:
+    pass
+
 from click_aliases import ClickAliasedGroup
 
 import click
